d095c2b16d18c8376e5343e468cbed209239cbd6
NNa97 <namdn25012003@gmail.com>
2024-03-06 13:51:59 +0700

    connetion database

---
+++ WebApplication1/src/java/dal/TimeSlotDBContext.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import entity.TimeSlot;
+import java.util.ArrayList;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author USER
+ */
+public class TimeSlotDBContext extends DBContext<TimeSlot> {
+
+
+    public ArrayList<TimeSlot> list() {
+        ArrayList<TimeSlot> slots = new ArrayList<>();
+        try {
+
+            String sql = "SELECT tid,tname FROM TimeSlot";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                TimeSlot t = new TimeSlot();
+                t.setId(rs.getInt("tid"));
+                t.setName(rs.getString("tname"));
+                slots.add(t);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TimeSlotDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return slots;
+    }
+
+   
+
+}
